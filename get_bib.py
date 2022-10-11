fd4b72168db47abdc8933e62f621c69a2c9a501b
1496nuonuo <bibo@mail.ustc.edu.cn>
2022-10-11 21:41:33 +0800

    fix :-换行问题、arxiv问题、问号句号问题

---
+++ get_bib.py
@@ -0,0 +1,31 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+def download_bib(title,num):
+    #num str
+    url = 'https://dblp1.uni-trier.de/search'
+    data = {
+        'q': title
+    }
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko)\
+    Chrome/55.0.2883.87 Safari/537.36'}
+    response = requests.get(url, params=data , headers = headers)
+    # print(response.text)
+    bs = BeautifulSoup(response.content, "lxml")
+    bib = bs.find(name ="nav",class_ = "publ")
+    li = bib.find(name="li",class_="drop-down").next_sibling
+    a = li.find(name="a")
+    bib_url =a.get('href')
+    response_bib = requests.get(bib_url , headers = headers)
+    bs_bib = BeautifulSoup(response_bib.content, "lxml") 
+    div_bib =bs_bib.find(name ="div",id ="main")
+    a_bib = div_bib.find(name ="div",id ="breadcrumbs")
+    b=a_bib.find_next_sibling()
+    c=b.find(name="a")
+    bib_download_url =c.get('href')
+    # print(bib_download_url)
+    r = requests.get(bib_download_url)
+    if not os.path.exists("./bib_repostitory"):
+        os.mkdir("./bib_repostitory")
+    with open("./bib_repostitory/"+num+".bib", "wb") as code:
+        code.write(r.content)
--- main.py
+++ main.py
@@ -1,12 +1,44 @@
 import pdfplumber
 import re
+from get_bib import download_bib
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+def del_pozhehao(mystr):
+    import enchant
+    import re
+    temp2 = mystr.replace('\n', ' ').strip()  # 删去换行
+    temp3 = temp2.replace('ﬁ', 'fi')  # 修改合字，否则搜索时搜索不出
+    temp1 = re.findall('([\s]([\S]*?)-\s([\S]*?)\s){1}', temp3, re.S)  # 找标题内容
+    temp4 = re.findall('^(([\S]*?)-\s([\S]*?)\s){1}', temp3, re.S)   #开头
+    temp5 = re.findall('([\s]([\S]*?)-\s([\S]*?)){1}$', temp3, re.S)  #结尾
+    d = enchant.Dict("en_US")
+    if len(temp4) != 0:
+        if d.check(temp4[0][1] + temp4[0][2]):
+            temp3_out = temp3.replace(temp4[0][0], temp4[0][1] + temp4[0][2] + ' ')
+        else:
+            temp3_out = temp3.replace(temp4[0][0], temp4[0][1] + ' ' + temp4[0][2] + ' ')
+    else:
+        temp3_out = temp3
+    if len(temp5) != 0:
+        if d.check(temp5[0][1] + temp5[0][2]):
+            temp4_out = temp3_out.replace(temp5[0][0], ' ' + temp5[0][1] + temp5[0][2])
+        else:
+            temp4_out = temp3_out.replace(temp5[0][0], ' ' + temp5[0][1] + '-' + temp5[0][2])
+    else:
+        temp4_out = temp3_out
+    temps = temp4_out
+    for ind in range(len(temp1)):
+        if d.check(temp1[ind][1] + temp1[ind][2]):
+            temps = temps.replace(temp1[ind][0], ' ' + temp1[ind][1] + temp1[ind][2] + ' ')
+        else:
+            temps = temps.replace(temp1[ind][0], ' ' + temp1[ind][1] + '-' + temp1[ind][2] + ' ')
+    return temps
 def find_title(bib_str):
     "对于引用文献取出标题，作为搜索"
-    temp1 = re.findall('[a-z]\.(.*?)[\.\?][\s+][^a-z]', bib_str, re.S)  # 找标题内容
-    temp2 = temp1[0].replace('\n', ' ').strip()  # 删去换行
-    title = temp2.replace('ﬁ', 'fi')  # 修改合字，否则搜索时搜索不出
+    temp1 = re.findall('[a-z]\.(.*?)(\.|\?)[\s+]([^a-z]|arXiv)', bib_str, re.S)  # 找标题内容
+    temp2 = temp1[0][0].replace('\n', ' ').strip()  # 删去换行
+    temp3 = temp2.replace('ﬁ', 'fi')  # 修改合字，否则搜索时搜索不出
+    title = del_pozhehao(temp3)
     return title
 
 # Press the green button in the gutter to run the script.
@@ -21,7 +53,7 @@ if __name__ == "__main__":
         #         #page.extract_text()函数即读取文本内容，下面这步是去掉文档最下面的页码
         #         page_content = '\n'.join(page.extract_text().split('\n')[:-1])
         #         content = content + page_content
-        for i in range(7, len(pdf.pages)):
+        for i in range(len(pdf.pages)-3,len(pdf.pages)):
             page = pdf.pages[i]
             bounding_box_left = (0, 0, pdf.pages[0].width / 2, pdf.pages[0].height)
             page_left = pdf.pages[i].crop(bounding_box_left)
@@ -40,11 +72,14 @@ if __name__ == "__main__":
             content = content + page_content
         regex = r'(?<=[Rr]eferences\n).*'
         s = re.findall(regex, content, re.S)  # 找reference之后的内容
-        print(s[0])
+        # print(s[0])
         m = re.findall('\](.*?)\[', s[0], re.S)  # 找][之间的内容]
         k = s[0].split(']')  # 取最后一个bib
         m.append(k[-1])
         d = list(map(find_title, m))
+        print(d)
+        for num in range(len(d)):
+            download_bib(d[num],str(num))
     #     如：s1 = 'Youtube-8m: A large-scale video classiﬁcation benchmark'
     #     s2 = 'Youtube-8m: A large-scale video classification benchmark'
 
